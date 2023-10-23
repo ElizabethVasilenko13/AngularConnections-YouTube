c0c5ee1eb5bf9620d170e353d9403fc64736606f
Yelyzaveta Vasylenko <liza.vasilenko13@gmail.com>
2023-10-23 20:10:47 +0200

    refactor: add components

--- youtube-client-app/src/app/app.module.ts
+++ youtube-client-app/src/app/app.module.ts
@@ -3,9 +3,29 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './components/header/header.component';
+import { SearchComponent } from './components/search/search.component';
+import { FilterComponent } from './components/header/filter/filter.component';
+import { SearchResultsComponent } from './components/search/search-results/search-results.component';
+import { SearchItemComponent } from './components/search/search-item/search-item.component';
+import { SortingComponent } from './components/sorting/sorting.component';
+import { AuthorizationComponent } from './components/authorization/authorization.component';
+import { RegistrationComponent } from './components/authorization/registration/registration.component';
+import { MainComponent } from './components/main/main.component';
 
 @NgModule({
-  declarations: [AppComponent],
+  declarations: [
+    AppComponent,
+    HeaderComponent,
+    SearchComponent,
+    FilterComponent,
+    SearchResultsComponent,
+    SearchItemComponent,
+    SortingComponent,
+    AuthorizationComponent,
+    RegistrationComponent,
+    MainComponent
+  ],
   imports: [BrowserModule, AppRoutingModule],
   providers: [],
   bootstrap: [AppComponent],
