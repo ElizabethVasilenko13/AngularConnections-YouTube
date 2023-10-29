3b29be23abdadbb3bf8b079d02d6f39cc698f19f
Yelyzaveta Vasylenko <liza.vasilenko13@gmail.com>
2023-10-29 21:02:32 +0100

    feat: add button component, change project structure

--- youtube-client-app/src/app/app.module.ts
+++ youtube-client-app/src/app/app.module.ts
@@ -4,27 +4,26 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './components/header/header.component';
-import { SearchComponent } from './components/search/search.component';
-import { FilterComponent } from './components/header/filter/filter.component';
-import { SearchResultsComponent } from './components/search/search-results/search-results.component';
-import { SearchItemComponent } from './components/search/search-item/search-item.component';
+import { SearchResultsComponent } from './components/main/search-results/search-results.component';
+import { SearchItemComponent } from './components/main/search-item/search-item.component';
 import { SortingComponent } from './components/sorting/sorting.component';
 import { AuthorizationComponent } from './components/authorization/authorization.component';
 import { RegistrationComponent } from './components/authorization/registration/registration.component';
 import { MainComponent } from './components/main/main.component';
+import { ButtonComponent } from './components/button/button.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     HeaderComponent,
-    SearchComponent,
-    FilterComponent,
+    SearchItemComponent,
     SearchResultsComponent,
     SearchItemComponent,
     SortingComponent,
     AuthorizationComponent,
     RegistrationComponent,
-    MainComponent
+    MainComponent,
+    ButtonComponent
   ],
   imports: [BrowserModule, AppRoutingModule],
   providers: [],
