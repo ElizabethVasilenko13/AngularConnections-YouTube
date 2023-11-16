ff8fbebf5c75a743e34eb31cf7024fc6a116fdd3
Yelyzaveta Vasylenko <liza.vasilenko13@gmail.com>
2023-11-16 15:39:44 +0100

    refactor: add return type

--- youtube-client-app/src/app/auth/guards/guards.guard.ts
+++ youtube-client-app/src/app/auth/guards/guards.guard.ts
@@ -2,7 +2,7 @@ import { inject } from '@angular/core';
 import { CanActivateFn, Router } from '@angular/router';
 import { AuthService } from '../services/auth.service';
 
-export const AuthGuard: CanActivateFn = () => {
+export const AuthGuard: CanActivateFn = (): boolean => {
   const authService = inject(AuthService);
   const router = inject(Router);
   if (!authService.isLoggedIn()) {
